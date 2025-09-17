9fa45868afe9c2ba61fa16b5d0e29d777f9f1c49
yu-paz <yuvalpaz2005@gmail.com>
2025-09-17 18:59:33 -0400

    Update TestStatusCheckPR.java
    commenting out test annotation

--- test/TestStatusCheckPR.java
+++ test/TestStatusCheckPR.java
@@ -26,7 +26,7 @@ public class TestStatusCheckPR {
     private static final String SUCCESS = "success";
     private static final String APPROVED = "APPROVED";
     
-    @Test
+    //@Test
     public void testPullRequest() throws Exception {
         String baseApiPath = getBaseApiPath();
         String toCurl = baseApiPath + "pulls?state=all";
